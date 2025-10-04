df52c9b932533940c43dffbac6e04d6df38716ef
tbn1303 <tbn22@scarletmail.rutgers.edu>
2025-10-04 17:29:22 -0400

    Update memgrind.c

--- memgrind.c
+++ memgrind.c
@@ -28,15 +28,27 @@ void task_B(){
 //Average runtime for each workload after execute 50 times
 double run_time(void *(func())){
         struct timeval start, end;
-        long total_time = 0;
+        double total_time = 0.0;
 
         for(int i = 0; i < RUNS; i++){
                 gettimeofday(&start, NULL);
                 func();
                 gettimeofday(&end, NULL);
 
-                total_time += (end.tv_sec - start.tv_sec) * 1000000L + (end.tv_usec - start.tv_usec);
+                long seconds = end.tv_sec - start.tv_sec;
+                long microseconds = end.tv_usec - start.tv_sec;
+
+                total_time += seconds + microseconds * 1e-6;
         }
 
-        return (double)total_time / RUNS;
+        return total_time / RUNS;
+}
+
+int main(int argc, char **argv){
+        srand(time(NULL));
+
+        printf("Task A avg time: %.6f secs\n", run_time(Task_A));
+        printf("Task B avg time: %.6f secs\n", run_time(Task_B));
+
+        return EXIT_SUCCESS;
 }
