f0cd7eed800401c5cc07e73be5a2720870a66b96
tbn1303 <tbn22@scarletmail.rutgers.edu>
2025-10-05 23:23:05 -0400

    Update memgrind.c

--- memgrind.c
+++ memgrind.c
@@ -100,30 +100,69 @@ void task_4(void){
         }
 }
 
-//Average runtime for each workload after execute 50 times
-double run_time(void (*func)(void)){
-        struct timeval start, end;
-        double total_time = 0.0;
+//Fill the heap, deallocate randomly half object
+void task_4(void){
+	char *obj[64];
 
-        for(int i = 0; i < RUNS; i++){
-                gettimeofday(&start, NULL);
-                func();
-                gettimeofday(&end, NULL);
+	//Fill the heap
+	for(int i = 0; i < 64; i++){
+		obj[i] = malloc(56);
+	}
 
-                long seconds = end.tv_sec - start.tv_sec;
-                long microseconds = end.tv_usec - start.tv_usec;
+	//Deallocate random half object 
+	int total_freed = 0;
+        while(total_freed < 32){
+                int index = rand() % 64;
 
-                total_time += seconds + microseconds * 1e-6;
+                if(obj[index]){
+                        free(obj[index]);
+                        obj[index] = NULL;
+                        total_freed++;
+                }
         }
 
-        return total_time / RUNS;
+	//Deallocate remaining objects
+	for(int i = 0; i < 64; i++){
+		if(obj[i]){
+			free(obj[i]);
+		}
+	}
+}
+
+//Fill heap, deallocate first 10 objects and allocate back 10 object with smaller size
+void task_5(void){
+	char *obj[64];
+
+	//Fill heap
+	for(int i = 0; i < 64; i++){
+		obj[i] = malloc(56);
+	}
+
+	//Deallocate first 10 objects
+	for(int i = 0; i < 10; i++){
+		free(obj[i]);
+		obj[i] = NULL;
+	}
+
+	//Reallocate 10 objects with smaller size
+	for(int i = 0; i < 10; i++){
+		obj[i] = malloc(24);
+	}
+
+	//Deallocate all objects
+	for(int i = 0; i < 64; i++){
+		free(obj[i]);
+	}
 }
 
 int main(int argc, char **argv){
         srand(time(NULL));
 
-        printf("Task A avg time: %.6f secs\n", run_time(task_1));
-        printf("Task B avg time: %.6f secs\n", run_time(task_2));
+        printf("Task 1 avg time: %.6f secs\n", run_time(task_1));
+	printf("Task 2 avg time: %.6f secs\n", run_time(task_2));
+	printf("Task 3 avg time: %.6f secs\n", run_time(task_3));
+	printf("Task 4 avg time: %.6f secs\n", run_time(task_4));
+	printf("Task 5 avg time: %.6f secs\n", run_time(task_5));
 
         return EXIT_SUCCESS;
 }
